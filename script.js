bdfc28ecbb007aff587d66746c74beff163d8500
lvv5040 <66978068+lvv5040@users.noreply.github.com>
2020-08-04 19:45:38 -0600

    added ajax one and two

--- script.js
+++ script.js
@@ -6,6 +6,35 @@
 
 //we need an event listener for the input
 //we need to make ajaxcalls to the API
+
+$( document ).ready(function() {
+    function ajaxone () {
+        var api_url = '' // need to get
+        var key = '' // need to get
+        $.ajax({
+            url: api_url + "?key=" + key + " &q=" + $( "#searchBar" ).val(),
+            contentType: "application/json",
+            dataType: 'json',
+            success: function(result){
+                console.log(result);
+            }
+        })
+    }
+
+    function ajaxtwo () {
+        var api_url = '' // need to get
+        var key = '' // need to get
+        $.ajax({
+            url: api_url + "?key=" + key + " &q=" + $( "#searchBar" ).val(),
+            contentType: "application/json",
+            dataType: 'json',
+            success: function(result){
+                console.log(result);
+            }
+        })
+    }
+  });
+
 //we need to append the desired results to the recipe and store
 //we need to animate the fridge
 //we need a function to clear old results
