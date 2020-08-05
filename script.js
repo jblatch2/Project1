b6b02f2f4a0479cdf28af16252f1836165cbd250
Paul <paulelliottwork@gmail.com>
2020-08-04 19:39:37 -0600

    shakin the fridge

--- script.js
+++ script.js
@@ -10,17 +10,25 @@ fridge();
 
 
 
-
-
-
-
-
 })
 
 //we need to make ajaxcalls to the API
 //we need to append the desired results to the recipe and store
 //we need to animate the fridge
 function fridge(){
-
+    anime({
+        targets: '.fridge',
+        keyframes: [
+          {rotate: -10},
+          {rotate: 10},
+          {rotate: -5},
+          {rotate: 5},
+          {rotate: 0}
+    
+        ],
+        duration: 1000,
+        easing: 'easeOutElastic(1, .8)',
+        loop: false
+      });
 }
 //we need a function to clear old results
